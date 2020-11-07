8132b0d63f91f6548e00b2fbab35c097b4adf051
Anewil <bartekwwb@gmail.com>
2020-11-07 20:19:26 +0100

    Updated network

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class Dataset:
         self.number_of_patterns = int(lines.pop(0))
         self.horizontal_resolution = int(lines.pop(0))
         self.vertical_resolution = int(lines.pop(0))
+        self.shape = self.horizontal_resolution * self.vertical_resolution
         for index in range(self.number_of_patterns):
             letter = lines.pop(0)
             pattern = ''
@@ -37,7 +38,75 @@ class Dataset:
             self.patterns.append(Pattern(letter, pattern))
 
 
+class Network:
+
+    def __init__(self, train_dataset):
+        self.train_dataset = train_dataset
+        self.letters = self.get_letters()
+        self.input_neurons = self.dataset_to_inputs(self.train_dataset)
+        self.layers = self.init_layers()
+
+    def get_letters(self):
+        letters = []
+        for pattern in self.train_dataset.patterns:
+            letters.append(pattern.letter)
+        return letters
+
+    def test(self, test_dataset):
+        for test in test_dataset.patterns:
+            input_neuron = InputNeron(test.value)
+            self.input_neurons = [input_neuron]
+            for layer in self.layers:
+                print(f'Letter {test.letter}')
+                outputs = self.evaluate_layer(layer)
+                best_confidence = max(outputs)
+                best_letter = self.letters[outputs.index(best_confidence)]
+                print(f'network has recognized letter {best_letter} with level of confidence = {round(best_confidence, 2)}')
+
+    def evaluate_layer(self, layer):
+        outputs = []
+        for input in self.input_neurons:
+            for neuron in layer:
+                output = neuron.compute_output(input.inputs)
+                print(output)
+                outputs.append(output)
+        return outputs
+
+    def init_layers(self):
+        layer = []
+        for input in range(len(self.input_neurons)):
+            weights = self.input_neurons[input].inputs.value
+            layer.append(Neuron(weights))
+        return [layer]
+
+    def dataset_to_inputs(self, dataset):
+        input_neurons = []
+        patterns = dataset.patterns
+        for pattern in patterns:
+            input_neurons.append(InputNeron(pattern))
+        return input_neurons
+
+
+class InputNeron:
+
+    def __init__(self, inputs):
+        self.inputs = inputs
+
+
+class Neuron:
+
+    def __init__(self, weights):
+        self.number_of_inputs = len(weights)
+        self.weights = weights
+
+    def compute_output(self, inputs):
+        value = 0
+        for i in range(self.number_of_inputs):
+            value += self.weights[i] * inputs[i]
+        return value
+
+
 train_dataset = Dataset('train.txt')
-patterns = train_dataset.patterns
-for pattern in patterns:
-    print(pattern.value)
+test_dataset = Dataset('test.txt')
+network = Network(train_dataset)
+network.test(test_dataset)
